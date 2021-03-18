e66d3e71f09ebb910dbe20795310066f9239326f
Dreamcatcher <shmilycowboy@163.com>
2021-03-18 22:58:18 +0800

    new

--- 画三角形.py
+++ 画三角形.py
@@ -1,8 +1,14 @@
-while True:
-    height=int(input('input the height:'))
+height = int(input('input the height:'))
+if height % 2 == 0:
+    print('input error!')
+else:
     for i in range(height):
-        for blank in range(1,height-i):
-            print(' ',end='')
+        for blank in range(1, height-i):
+            print(' ', end='')
         for j in range(i*2+1):
-            print('*',end='')
+            print('*', end='')
         print()
+    print('-------------------------------------------------')
+    for i in range(height):
+        print(' '*(height-i-1), end='')
+        print('*'*(i*2+1))
